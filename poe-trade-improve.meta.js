1fc20a126398175eab3599a25dd0ae21fabc8d49
Hopfenbluetentee <hopfenbluetentee.gilde@gmail.com>
2026-08-10 22:49:42 +0200

    Create poe-trade-improve.meta.js

---
+++ poe-trade-improve.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         PoE Trade Improve
+// @namespace    poe-trade-improve
+// @version      5.3
+// @description  Compact mirror bar for the PoE trade search filters
+// @match        https://www.pathofexile.com/trade/search/*
+// @grant        none
+// @run-at       document-idle
+// @updateURL    https://github.com/Hopfenbluetentee/poe-trade-improve/raw/refs/heads/main/poe-trade-improve.meta.js
+// @downloadURL  https://github.com/Hopfenbluetentee/poe-trade-improve/raw/refs/heads/main/poe-trade-improve.user.js
+// ==/UserScript==
